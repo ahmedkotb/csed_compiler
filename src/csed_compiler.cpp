d227da023d0c278c9d02d405b19cfa5d8fb538fc
ahmed.kotb <ahmed.kotb@4861c95f-93c2-46ff-99fd-224d39b3984d>
2011-04-20 18:53:22 +0000

    initial commit contains -a cpp file that just prints a string to screen -a readme file with our names
    git-svn-id: https://subversion.assembla.com/svn/csed_compiler/trunk@2 4861c95f-93c2-46ff-99fd-224d39b3984d

---
+++ src/csed_compiler.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : csed_compiler.cpp
+// Author      : Ahmed Kotb
+// Version     :
+// Copyright   : All Rights Reserved KotbCorp
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "The Best Compiler isA" << endl;
+	return 0;
+}
