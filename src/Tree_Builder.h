64414da1b017ea92f9d79491267c7a2e88db23ed
eng.a.emara <eng.a.emara@4861c95f-93c2-46ff-99fd-224d39b3984d>
2011-04-22 21:53:27 +0000

    This is nearly final version of parser (Test is the reaming part)
    git-svn-id: https://subversion.assembla.com/svn/csed_compiler/trunk@9 4861c95f-93c2-46ff-99fd-224d39b3984d

---
+++ src/Tree_Builder.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   Tree_Builder.h
+ * Author: 3mara
+ *
+ * Created on April 21, 2011, 11:26 PM
+ */
+
+#ifndef TREE_BUILDER_H
+#define	TREE_BUILDER_H
+
+
+#include<stack.h>
+#include <vector>
+#include <string.h>
+#include "NFA.h"
+//Constants
+#define RE 0
+#define RD 1
+//Operators
+#define OR "|"
+#define CONCATINATE " "
+#define CLOSURE "*"
+#define POSITIVE_CLOSURE "+"
+using namespace std;
+
+class Tree_Builder {
+public:
+    stack <string> op_Stack;
+    vector <string>output;
+    vector <string>input;
+    Tree_Builder(vector <string> in);
+    Tree_Builder();
+    virtual ~Tree_Builder();
+    void gotOper(string opThis, int prec1);
+    void gotParen(string ch);
+    void scan();
+    void construct_NFA(vector <NFA> in);
+    bool isOperator(string token);
+    NFA stringToNFA(vector <string> in);
+private:
+    map<string,vector<string> > RDMap;
+    stack <NFA> operand_stack;
+
+};
+
+#endif	/* TREE_BUILDER_H */
+
