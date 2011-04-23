00b9dbb276312b762887542adfd03f6be2d7241d
shakes <shakes@4861c95f-93c2-46ff-99fd-224d39b3984d>
2011-04-23 20:04:30 +0000

    Primary DFA Class (Not complete)
    git-svn-id: https://subversion.assembla.com/svn/csed_compiler/trunk@24 4861c95f-93c2-46ff-99fd-224d39b3984d

---
+++ src/DFA.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   DFA.cpp
+ * Author: Ahmed El-Sharkasy
+ * 
+ * Created on 23 أبريل, 2011, 09:49 م
+ */
+
+#include "DFA.h"
+
+DFA::DFA() {
+}
+
+DFA::DFA(const DFA& orig) {
+}
+
+DFA::~DFA() {
+}
+
---
+++ src/DFA.h
@@ -0,0 +1,40 @@
+/* 
+ * File:   DFA.h
+ * Author: Ahmed El-Sharkasy
+ *
+ * Created on 23 أبريل, 2011, 09:49 م
+ */
+
+#ifndef DFA_H
+#define	DFA_H
+
+#include "NFA_State.h"
+
+class DFA {
+public:
+    DFA();
+    DFA(const DFA& orig);
+    virtual ~DFA();
+
+    // mov method which takes a state and input character and returns a vector of the transitions
+    vector<NFA_State *> * mov(NFA_State* current_state, char input_character);
+
+    // create a DFA state representing the given NFA states and sets the accepting pattern of the DFA state
+    create_DFA_state(NFA_State* states);
+private:
+
+    NFA_State * start_state;
+
+    //holds all input alphabet
+    vector <char> alphabet;
+
+    //hashmap returns a DFA state representing the given ids of NFA states
+    multimap <vector<int*>, NFA_State*> states_IDS;
+
+    //vector for all DFA states
+    vector<NFA_State*>* all_states;
+
+};
+
+#endif	/* DFA_H */
+
