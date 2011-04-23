a24161e9de144938fcded83a8d0986b0edc45a32
eng.a.emara <eng.a.emara@4861c95f-93c2-46ff-99fd-224d39b3984d>
2011-04-23 16:24:58 +0000

    This is nearly final version of parser (Test is the reaming part)
    git-svn-id: https://subversion.assembla.com/svn/csed_compiler/trunk@18 4861c95f-93c2-46ff-99fd-224d39b3984d

--- src/Tree_Builder.h
+++ src/Tree_Builder.h
@@ -39,7 +39,7 @@ public:
     NFA createByBaseCase(string str);
     NFA stringToNFA(vector <string> *in);
 private:
-    map<string, vector<string> > keyWordMap;
+    map<string, string > keyWordMap;
     map<string, vector<string> > RDMap;
     stack <NFA> operand_stack;
 
