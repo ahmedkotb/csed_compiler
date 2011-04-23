2ca820f1b514f11b1bb084e12689698092eb8c55
eng.a.emara <eng.a.emara@4861c95f-93c2-46ff-99fd-224d39b3984d>
2011-04-23 18:30:55 +0000

    This is nearly final version of parser (Test is the reaming part)
    git-svn-id: https://subversion.assembla.com/svn/csed_compiler/trunk@21 4861c95f-93c2-46ff-99fd-224d39b3984d

---
+++ src/Parser.h
@@ -0,0 +1,53 @@
+/* 
+ * File:   Tree_Builder.h
+ * Author: 3mara
+ *
+ * Created on April 21, 2011, 11:26 PM
+ */
+
+#ifndef PARSER_H
+#define	PARSER_H
+
+
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <vector>
+#include <string>
+#include "NFA.h"
+//Constants
+#define RE 0
+#define RD 1
+//Operators
+#define OR "|"
+#define CONCATINATE " "
+#define CLOSURE "*"
+#define POSITIVE_CLOSURE "+"
+#define SPECIAL_NUM 11
+#define CONCATINATION_SYMBOL " "
+#define OPERATOR_NUM 4
+using namespace std;
+
+class Parser {
+public:
+    stack <string> op_Stack;
+    vector <string>output;
+    Parser(vector <string> in);
+    vector <string> input;
+    Parser();
+    virtual ~Parser();
+    void gotOper(string opThis, int prec1);
+    void gotParen(string ch);
+    void scan();
+    bool isOperator(string token);
+    NFA createByBaseCase(string str);
+    NFA stringToNFA(vector <string> *in);
+    void split(string line);
+    bool isSpecialChar(char c);
+private:
+    map<string, string > keyWordMap;
+    map<string, vector<string> > RDMap;
+    stack <NFA> operand_stack;
+};
+
+#endif	/* TREE_BUILDER_H */
